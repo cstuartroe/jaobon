7fcaac91e3eb0b6879c65052dc8f5ee4c321825c
Conor Stuart-Roe <conorstuartroe@gmail.com>
2025-08-01 15:32:21 -0400

    Convert AnnotatedCharacter to functional component

--- src/AnnotatedText.tsx
+++ src/AnnotatedText.tsx
@@ -1,367 +1,348 @@
-import React, { Component } from "react";
-import { Root, ROOTS } from "./roots";
+import React, {useState} from "react";
+import {Root, ROOTS} from "./roots";
 import {stringToSyllable, syllableToDots} from "./syllables";
 import classNames from "classnames";
 import {WritingSystem, DisplaySettings} from "./DisplaySettings";
 
 type ParsingError = {
-    message: string;
+  message: string;
 }
 
 export function isError(x: any): x is ParsingError {
-    return x.message !== undefined;
+  return x.message !== undefined;
 }
 
-function capitalize(s: string): string {
-    return s[0].toUpperCase() + s.slice(1);
+function capitalizeWord(s: string): string {
+  return s[0].toUpperCase() + s.slice(1);
 }
 
 function languageClassName(writingSystem: WritingSystem) {
-    if (writingSystem === "texting") {
-        return "roman";
-    }
-    return writingSystem;
+  if (writingSystem === "texting") {
+    return "roman";
+  }
+  return writingSystem;
 }
 
 type ACProps = {
-    root: Root,
-    displaySettings: DisplaySettings,
-    capitalize?: boolean,
+  root: Root,
+  displaySettings: DisplaySettings,
+  capitalize?: boolean,
 }
 
-type ACState = {
-    tooltipShown: boolean,
-}
-
-export class AnnotatedCharacter extends Component<ACProps, ACState> {
-  constructor(props: ACProps) {
-    super(props);
-    this.state = {
-        tooltipShown: false,
-    };
-  }
-
-  annotated() {
-      switch (this.props.displaySettings.writingSystem) {
-          case "roman":
-              if (this.props.capitalize) {
-                  return capitalize(this.props.root.syllable);
-              } else {
-                  return this.props.root.syllable;
-              }
-          case "texting":
-              return this.props.root.singleConsonant || this.props.root.syllable;
-          case "cjk":
-              return this.props.root.CJK;
-          case "dots":
-              const syllable = stringToSyllable(this.props.root.syllable);
-              if (syllable === null) {
-                  return undefined;
-              }
-              return syllableToDots(syllable);
+function annotated({root, displaySettings, capitalize}: ACProps) {
+  switch (displaySettings.writingSystem) {
+    case "roman":
+      if (capitalize) {
+        return capitalizeWord(root.syllable);
+      } else {
+        return root.syllable;
       }
+    case "texting":
+      return root.singleConsonant || root.syllable;
+    case "cjk":
+      return root.CJK;
+    case "dots":
+      const syllable = stringToSyllable(root.syllable);
+      if (syllable === null) {
+        return undefined;
+      }
+      return syllableToDots(syllable);
   }
+}
 
+export function AnnotatedCharacter(props: ACProps) {
+  const [tooltipShown, setTooltipShown] = useState(false);
 
-  render() {
-      return (
-          <span
-              className={classNames([
-                  "annotated",
-                  languageClassName(this.props.displaySettings.writingSystem),
-                  {"tooltip-shown": this.state.tooltipShown}
-              ])}
-              onMouseEnter={() => this.setState({tooltipShown: true})}
-              onMouseLeave={() => this.setState({tooltipShown: false})}
-          >
-              <a
-                  href={`/roots#${this.props.root.syllable}`}
-                  target="_blank"
-              >
-                  {this.annotated()}
-              </a>
-              {this.state.tooltipShown && (
-                  <span className="translation">
-                      <b>{this.props.root.syllable}</b>:{' '}
-                      {this.props.root.definition}
-                  </span>
-              )}
-          </span>
-    );
-  }
+  return (
+    <span
+      className={classNames([
+        "annotated",
+        languageClassName(props.displaySettings.writingSystem),
+        {"tooltip-shown": tooltipShown}
+      ])}
+      onMouseEnter={() => setTooltipShown(true)}
+      onMouseLeave={() => setTooltipShown(false)}
+    >
+      <a
+        href={`/roots#${props.root.syllable}`}
+        target="_blank"
+      >
+        {annotated(props)}
+      </a>
+      {tooltipShown && (
+        <span className="translation">
+          <b>{props.root.syllable}</b>:{' '}
+          {props.root.definition}
+        </span>
+      )}
+    </span>
+  );
 }
 
 type ProperNoun = {
-    roots: Root[],
+  roots: Root[],
 }
 
 type Literal = {
-    literal: string;
+  literal: string;
 }
 
 export function isPN(x: ProperNoun | Root): x is ProperNoun {
-    return (x as ProperNoun).roots !== undefined;
+  return (x as ProperNoun).roots !== undefined;
 }
 
 export function isLiteral(x: any): x is Literal {
-    return (x as Literal).literal !== undefined;
+  return (x as Literal).literal !== undefined;
 }
 
-const ProperNounBrackets: {[key in WritingSystem]: [string, string]} = {
-    "cjk": ["《", "》"],
-    "dots": ["<", ">"],
-    "roman": ["", ""],
-    "texting": ["", ""],
+const ProperNounBrackets: { [key in WritingSystem]: [string, string] } = {
+  "cjk": ["《", "》"],
+  "dots": ["<", ">"],
+  "roman": ["", ""],
+  "texting": ["", ""],
 }
 
 type APNProps = {
-    pn: ProperNoun,
-    displaySettings: DisplaySettings,
+  pn: ProperNoun,
+  displaySettings: DisplaySettings,
 }
 
-class AnnotatedPN extends Component<APNProps, {}> {
-    constructor(props: APNProps) {
-        super(props);
-    }
+function AnnotatedPN({pn, displaySettings}: APNProps) {
+  const [lbr, rbr] = ProperNounBrackets[displaySettings.writingSystem];
 
-    render() {
-        let {displaySettings, pn} = this.props;
-        const [lbr, rbr] = ProperNounBrackets[displaySettings.writingSystem];
+  if (displaySettings.writingSystem === "texting") {
+    displaySettings = {...displaySettings, writingSystem: "roman"}
+  }
 
-        if (displaySettings.writingSystem === "texting") {
-            displaySettings = {...displaySettings, writingSystem: "roman"}
-        }
-
-        return (
-            <span className="proper-noun">
-                <span className={languageClassName(displaySettings.writingSystem)}>{lbr}</span>
-                {pn.roots.map((root, i) => (
-                    <AnnotatedCharacter root={root} displaySettings={displaySettings} key={i} capitalize={i === 0}/>
-                ))}
-                <span className={languageClassName(displaySettings.writingSystem)}>{rbr}</span>
-            </span>
-        );
-    }
+  return (
+    <span className="proper-noun">
+            <span className={languageClassName(displaySettings.writingSystem)}>{lbr}</span>
+      {pn.roots.map((root, i) => (
+        <AnnotatedCharacter root={root} displaySettings={displaySettings} key={i} capitalize={i === 0}/>
+      ))}
+      <span className={languageClassName(displaySettings.writingSystem)}>{rbr}</span>
+        </span>
+  );
 }
 
 const charMappings = new Map<string, [string, string]>([
-    // [Original, [CJK, Dots]]
-    [' ', ["\u200b", "\u200b"]],
-    ['!', ['！', '!']],
-    [',', ['，', '']],
-    ['~', ['〜', '.']],
-    ['.', ['。', '.']],
-    ['?', ['？', '?']],
-    ['(', ['（ ', '(']],
-    [')', ['）', ')']],
-    [':', ['：', ':']],
-    ['-', ['–', '-']],
-    ['“', ['「', '"']],
-    ['”', ['」', '"']],
-    ['…', ['…', '...']],
+  // [Original, [CJK, Dots]]
+  [' ', ["\u200b", "\u200b"]],
+  ['!', ['！', '!']],
+  [',', ['，', '']],
+  ['~', ['〜', '.']],
+  ['.', ['。', '.']],
+  ['?', ['？', '?']],
+  ['(', ['（ ', '(']],
+  [')', ['）', ')']],
+  [':', ['：', ':']],
+  ['-', ['–', '-']],
+  ['“', ['「', '"']],
+  ['”', ['」', '"']],
+  ['…', ['…', '...']],
 ]);
 
 export function parseJaobon(sentence: string): ParsingError | (Root | ProperNoun | Literal | string)[] {
-    const pieces: (Root | ProperNoun | Literal | string)[] = [];
+  const pieces: (Root | ProperNoun | Literal | string)[] = [];
 
-    let currentWord = "";
-    let currentProperNoun: ProperNoun | undefined = undefined;
-    let inLiteral = false;
+  let currentWord = "";
+  let currentProperNoun: ProperNoun | undefined = undefined;
+  let inLiteral = false;
 
-    function pushPiece(piece: Root | Literal | string) {
-        if (currentProperNoun === undefined) {
-            pieces.push(piece);
+  function pushPiece(piece: Root | Literal | string) {
+    if (currentProperNoun === undefined) {
+      pieces.push(piece);
+    } else {
+      if (typeof piece === "string") {
+        if (piece !== " ") {
+          return {message: "Tried to include punctuation in proper noun"};
+        }
+      } else if (isLiteral(piece)) {
+        throw new Error("Proper noun cannot contain literal")
+      } else {
+        currentProperNoun.roots.push(piece);
+      }
+    }
+  }
+
+  function popWord() {
+    if (currentWord !== "") {
+      if (inLiteral) {
+        pushPiece({literal: currentWord});
+      } else {
+        const root = ROOTS.get(currentWord);
+        if (root === undefined) {
+          console.error(`Unrecognized syllable: ${currentWord}`)
         } else {
-            if (typeof piece === "string") {
-                if (piece !== " ") {
-                    return {message: "Tried to include punctuation in proper noun"};
-                }
-            } else if (isLiteral(piece)) {
-                throw new Error("Proper noun cannot contain literal")
-            } else {
-                currentProperNoun.roots.push(piece);
-            }
+          pushPiece(root);
         }
+      }
+      currentWord = "";
     }
+  }
 
-    function popWord() {
-        if (currentWord !== "") {
-            if (inLiteral) {
-                pushPiece({literal: currentWord});
-            } else {
-                const root = ROOTS.get(currentWord);
-                if (root === undefined) {
-                    console.error(`Unrecognized syllable: ${currentWord}`)
-                } else {
-                    pushPiece(root);
-                }
-            }
-            currentWord = "";
-        }
+  for (let i = 0; i < sentence.length; i++) {
+    const c = sentence[i];
+    if (c === '`') {
+      popWord();
+      inLiteral = !inLiteral;
+    } else if (inLiteral) {
+      currentWord += c;
+    } else if (c.match(/[a-z]/) !== null) {
+      currentWord += c;
+    } else if (c === '[') {
+      if (currentProperNoun !== undefined) {
+        return {message: "Nested brackets"};
+      }
+      currentProperNoun = {
+        roots: [],
+      };
+      pieces.push(currentProperNoun);
+    } else if (c === ']') {
+      if (currentProperNoun === undefined) {
+        return {message: "Unmatched bracket"};
+      }
+      popWord();
+      currentProperNoun = undefined;
+    } else {
+      popWord();
+      pushPiece(c);
     }
+  }
+  popWord();
 
-    for (let i = 0; i < sentence.length; i++) {
-        const c = sentence[i];
-        if (c === '`') {
-            popWord();
-            inLiteral = !inLiteral;
-        } else if (inLiteral) {
-            currentWord += c;
-        } else if (c.match(/[a-z]/) !== null) {
-            currentWord += c;
-        } else if (c === '[') {
-            if (currentProperNoun !== undefined) {
-                return {message: "Nested brackets"};
-            }
-            currentProperNoun = {
-                roots: [],
-            };
-            pieces.push(currentProperNoun);
-        } else if (c === ']') {
-            if (currentProperNoun === undefined) {
-                return {message: "Unmatched bracket"};
-            }
-            popWord();
-            currentProperNoun = undefined;
-        } else {
-            popWord();
-            pushPiece(c);
-        }
-    }
-    popWord();
+  if (currentProperNoun !== undefined) {
+    return {message: "Unmatched brackets"};
+  }
 
-    if (currentProperNoun !== undefined) {
-        return {message: "Unmatched brackets"};
-    }
-
-    return pieces;
+  return pieces;
 }
 
-export type MultiscriptText = {roman: string, CJK: string}
+export type MultiscriptText = { roman: string, CJK: string }
 
 export function multiscriptText(sentence: string): ParsingError | MultiscriptText {
-    let roman = "";
-    let CJK = "";
-    const parsed = parseJaobon(sentence);
-    if (isError(parsed)) {
-        return parsed;
-    }
+  let roman = "";
+  let CJK = "";
+  const parsed = parseJaobon(sentence);
+  if (isError(parsed)) {
+    return parsed;
+  }
 
-    parsed.forEach((piece, i) => {
-        if (typeof piece === "string") {
+  parsed.forEach((piece, i) => {
+    if (typeof piece === "string") {
+      if (piece === '"') {
+        piece = (i > 0 && parsed[i - 1] !== " ") ? "”" : "“"
+      }
+
+      const pair = charMappings.get(piece);
+      if (pair === undefined) {
+        console.error(`Unknown char: ${piece}`);
+      } else {
+        roman += piece;
+        if (piece != " ") {
+          const [cjkPunct, dotsPunct] = pair;
+          CJK += cjkPunct;
+        }
+      }
+    } else if (isLiteral(piece)) {
+      roman += piece.literal;
+      CJK += piece.literal;
+    } else if (isPN(piece)) {
+      let romanPn = ""
+      CJK += ProperNounBrackets["cjk"][0];
+      piece.roots.forEach(root => {
+        romanPn += root.syllable;
+        CJK += root.CJK;
+      });
+      CJK += ProperNounBrackets["cjk"][1];
+      roman += capitalizeWord(romanPn);
+    } else {
+      roman += piece.syllable;
+      CJK += piece.CJK;
+    }
+  })
+
+  return {
+    roman,
+    CJK,
+  }
+}
+
+type Props = {
+  sentence: string,
+  displaySettings: DisplaySettings,
+  inline: boolean,
+}
+
+export default function AnnotatedText(props: Props): JSX.Element {
+  const parsed = parseJaobon(props.sentence);
+  if (isError(parsed)) {
+    return <span style={{color: "red"}}>{parsed.message}</span>;
+  }
+
+  return <span className={props.inline ? "inline" : ""}>
+        {parsed.map((piece, i) => {
+          if (typeof piece === "string") {
             if (piece === '"') {
-                piece = (i > 0 && parsed[i - 1] !== " ") ? "”" : "“"
+              piece = (i > 0 && parsed[i - 1] !== " ") ? "”" : "“"
             }
 
             const pair = charMappings.get(piece);
             if (pair === undefined) {
-                console.error(`Unknown char: ${piece}`);
+              console.error(`Unknown char: ${piece}`);
             } else {
-                roman += piece;
-                if (piece != " ") {
-                    const [cjkPunct, dotsPunct] = pair;
-                    CJK += cjkPunct;
-                }
+              const [cjkPunct, dotsPunct] = pair;
+              let punct: string = "";
+              switch (props.displaySettings.writingSystem) {
+                case "roman":
+                case "texting":
+                  punct = piece;
+                  break;
+                case "cjk":
+                  punct = cjkPunct;
+                  break;
+                case "dots":
+                  punct = dotsPunct;
+                  break;
+              }
+              return <span className={props.displaySettings.writingSystem} key={i}>{punct}</span>;
             }
-        } else if (isLiteral(piece)) {
-            roman += piece.literal;
-            CJK += piece.literal;
-        } else if (isPN(piece)) {
-            let romanPn = ""
-            CJK += ProperNounBrackets["cjk"][0];
-            piece.roots.forEach(root => {
-                romanPn += root.syllable;
-                CJK += root.CJK;
-            });
-            CJK += ProperNounBrackets["cjk"][1];
-            roman += capitalize(romanPn);
-        } else {
-            roman += piece.syllable;
-            CJK += piece.CJK;
-        }
-    })
-
-    return {
-        roman,
-        CJK,
-    }
-}
-
-type Props = {
-    sentence: string,
-    displaySettings: DisplaySettings,
-    inline: boolean,
-}
-
-export default function AnnotatedText(props: Props): JSX.Element {
-    const parsed = parseJaobon(props.sentence);
-    if (isError(parsed)) {
-        return <span style={{color: "red"}}>{parsed.message}</span>;
-    }
-
-    return <span className={props.inline ? "inline" : ""}>
-        {parsed.map((piece, i)=> {
-            if (typeof piece === "string") {
-                if (piece === '"') {
-                    piece = (i > 0 && parsed[i - 1] !== " ") ? "”" : "“"
-                }
-
-                const pair = charMappings.get(piece);
-                if (pair === undefined) {
-                    console.error(`Unknown char: ${piece}`);
-                } else {
-                    const [cjkPunct, dotsPunct] = pair;
-                    let punct: string = "";
-                    switch (props.displaySettings.writingSystem) {
-                        case "roman":
-                        case "texting":
-                            punct = piece;
-                            break;
-                        case "cjk":
-                            punct = cjkPunct;
-                            break;
-                        case "dots":
-                            punct = dotsPunct;
-                            break;
-                    }
-                    return <span className={props.displaySettings.writingSystem} key={i}>{punct}</span>;
-                }
-            } else if (isLiteral(piece)) {
-                return <span className={props.displaySettings.writingSystem} key={i}>{piece.literal}</span>;
-            } else if (isPN(piece)) {
-                return <AnnotatedPN pn={piece} displaySettings={props.displaySettings} key={i}/>;
-            } else {
-                return <AnnotatedCharacter root={piece} displaySettings={props.displaySettings} key={i}/>;
-            }
+          } else if (isLiteral(piece)) {
+            return <span className={props.displaySettings.writingSystem} key={i}>{piece.literal}</span>;
+          } else if (isPN(piece)) {
+            return <AnnotatedPN pn={piece} displaySettings={props.displaySettings} key={i}/>;
+          } else {
+            return <AnnotatedCharacter root={piece} displaySettings={props.displaySettings} key={i}/>;
+          }
         })}
     </span>
 }
 
 type TLProps = {
-    number?: number,
-    jaobon: string,
-    translation: string,
-    displaySettings: DisplaySettings,
+  number?: number,
+  jaobon: string,
+  translation: string,
+  displaySettings: DisplaySettings,
 }
 
 export function TranslatedLine(props: TLProps) {
-    return (
-        <div className="translated-line">
-            <p style={{position: "relative", marginBottom: ".5vh"}}>
-                {props.number && (
-                    <span style={{
-                        position: "absolute",
-                        left: 0,
-                        top: "50%",
-                        transform: "translate(-100%, -50%)",
-                        paddingRight: ".5vw",
-                    }}>
+  return (
+    <div className="translated-line">
+      <p style={{position: "relative", marginBottom: ".5vh"}}>
+        {props.number && (
+          <span style={{
+            position: "absolute",
+            left: 0,
+            top: "50%",
+            transform: "translate(-100%, -50%)",
+            paddingRight: ".5vw",
+          }}>
                         {props.number}.
                     </span>
-                )}
-                <AnnotatedText sentence={props.jaobon} displaySettings={props.displaySettings} inline={false}/>
-            </p>
-            <p>{(props.displaySettings.showTranslation === "show") && props.translation}</p>
-        </div>
-    );
+        )}
+        <AnnotatedText sentence={props.jaobon} displaySettings={props.displaySettings} inline={false}/>
+      </p>
+      <p>{(props.displaySettings.showTranslation === "show") && props.translation}</p>
+    </div>
+  );
 }
